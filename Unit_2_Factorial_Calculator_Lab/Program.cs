d97a87cb4e7649d0102e4b15efed689aebd095a0
harry <harry.nguyen.2016@gmail.com>
2023-04-13 11:33:56 -0400

    first commit

---
+++ Unit_2_Factorial_Calculator_Lab/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Factorial Calculator!\n");
+        string prompt = "y";
+        do
+        {
+            long factorial = 1;
+            Console.Write("Enter an integer (1-20): ");
+            int input = int.Parse(Console.ReadLine());
+            if (input < 1 || input > 20)
+                Console.WriteLine("Invaid Input, please try again.");
+            else
+            {
+                FactorialCalculator(input, ref factorial);
+                Console.WriteLine($"The factorial of {input} is {factorial}");
+            }
+
+            Console.Write("Continue? (y/n): ");
+            prompt = Console.ReadLine();
+        } while (prompt == "y");
+    }
+
+    static void FactorialCalculator(int input, ref long factorial)
+    {
+        if (input == 0)
+            return;
+        else
+        {
+            factorial *= input;
+            FactorialCalculator(input - 1, ref factorial);
+        }
+    }
+}
